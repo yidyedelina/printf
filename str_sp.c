197e9ddf96a4e2c7f4c37e949052fa6a7f417c86
yidyedelina <yidnekachewtebeje@gmail.com>
2022-04-16 17:16:26 +0300

    updates

--- str_sp.c
+++ str_sp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "string.h"
 #include "stdio.h"
 /**
  * _putc - a character specifier
@@ -25,16 +26,34 @@ int _putstr(char *s)
 	len = len + printstr(s);
 	return (len);
 }
+/**
+ * _putStr - print printable string
+ * @s: a string
+ * Return: always length;
+ */
 int _putStr(char *s)
 {
 	int len = 0;
+	char *p;
+
+	if (s == NULL)
+		s = "(null)";
 	while (*s != '\0')
 	{
 		if (*s > 0 && *s < 32)
 		{
 			putchar('\\');
 			putchar('x');
-			len = len + printstr(convert(*s, 16));
+			p = convert(*s, 16);
+			if (strlen(p) == 2)
+			{
+				len += printstr(s);
+			}
+			else
+			{
+				putchar('0');
+				putchar(*p);
+			}
 			len = len + 2;
 		}
 		else
